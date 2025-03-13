e9212fb61da722c7c0e193968384f2dcf6b72abb
Patrik Fabijanić <patrik.fabijanic9@gmail.com>
2025-03-13 12:24:37 +0100

    ne mogu skuzit zasto mi dolazi greksaaa

--- uploadImage.js
+++ uploadImage.js
@@ -1,26 +1,40 @@
 import express from 'express'
 import multer from 'multer'
+import { db } from './server.js'
+import { GridFSBucket } from 'mongodb'
+import { GridFsStorage } from 'multer-gridfs-storage'
 
 const router = express.Router()
 
 
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'uploads')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.originalname)
+const storage = new GridFsStorage({
+    db,
+    file: (req, file) => {
+      if (file.mimetype === 'image/jpeg') {
+        return {
+          filename: file.originalname,
+          bucketName: 'uploads',
+          
+        };
+      } else {
+        return null;
+      }
     }
-  })
-  
-  const upload = multer({storage})
-  
+  });
 
 
-router.post('/api/upload',upload.single('file'), (req, res) => {
-    res.send(req.file)
+
+  const upload = multer({ storage });
+  
+ 
+
+router.post('/api/upload',upload.single('file'), async (req, res) => {
+    if(!req.file) {
+        return res.status(400).json({message: "Nema dodane slike"})
+    }
+
+    res.status(200).json({message: "Slika je uspijesno dodana!"})
   })
 
 
